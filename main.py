5c82851a7257dfba722f002aeef44fab7753f8fc
itsshivangi <Shivangishivangi@my.unt.edu>
2026-03-31 15:57:06 -0500

    made a new csv file from the data file

--- main.py
+++ main.py
@@ -25,46 +25,65 @@
 #Here i used csv module to read the data and print only the temperature column. I created an empty list called Temperature and then looped through each row in the data. If the second element of the row (which is the temperature) is not equal to "temp" (the header), I converted it to an integer and appended it to the Temperature list. Finally, I printed the Temperature list.
       
 import pandas
-data = pandas.read_csv("weather_data.csv")
-print(data)
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
+# print(data)
+# print(data["temp"])
 
-data_dict = data.to_dict()
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# data["temp"].to_list()
+# print(len(data["temp"].to_list()))
+
+# average_temp = sum(data["temp"].to_list()) / len(data["temp"].to_list())
+# print(average_temp)
+
+# #OR 
+
+# print(data["temp"].mean())
+
+# max_temp = data["temp"].max()
+# print(max_temp)
+
+# #Get data in columns
+# print(data["condition"])
+# print(data.condition)
+
+# #Get data in rows
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+
+# monday_temp = monday.temp[0]
+# monday_temp_F = monday_temp * 9/5 + 32
+# print(monday_temp_F)
+
+# #CREATE A DATA FRAME FROM SCRATCH
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# print(data)
+# data.to_csv("new_data.csv")    
+
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(gray_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)    
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
 print(data_dict)
 
-data["temp"].to_list()
-print(len(data["temp"].to_list()))
-
-average_temp = sum(data["temp"].to_list()) / len(data["temp"].to_list())
-print(average_temp)
-
-#OR 
-
-print(data["temp"].mean())
-
-max_temp = data["temp"].max()
-print(max_temp)
-
-#Get data in columns
-print(data["condition"])
-print(data.condition)
-
-#Get data in rows
-print(data[data.day == "Monday"])
-print(data[data.temp == data.temp.max()])
-
-monday = data[data.day == "Monday"]
-print(monday.condition)
-
-monday_temp = monday.temp[0]
-monday_temp_F = monday_temp * 9/5 + 32
-print(monday_temp_F)
-
-#CREATE A DATA FRAME FROM SCRATCH
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
-data = pandas.DataFrame(data_dict)
-print(data)
-data.to_csv("new_data.csv")    
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
